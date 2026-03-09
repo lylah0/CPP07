26558090baf605fa1045aecf0618f299835bc415
lylah <lipam.randrizzello@gmail.com>
2026-03-09 16:39:57 +0100

    ex01 OK

--- ex01/Functions.hpp
+++ ex01/Functions.hpp
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*   functions.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lylrandr <lylrandr@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/03/03 12:30:45 by lylrandr          #+#    #+#             */
-/*   Updated: 2026/03/09 16:31:18 by lylrandr         ###   ########.fr       */
+/*   Created: 2026/03/09 16:17:38 by lylrandr          #+#    #+#             */
+/*   Updated: 2026/03/09 16:36:16 by lylrandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-#define ITER_HPP
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
 
-template <typename T, typename F>
-void	iter(T* array, const int len, F func){
-	for(int i = 0; i < len; i++){
-		func(array[i]);
-	}
+template <typename T>
+void	print(T c){
+	std::cout << c << std::endl;
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,21 @@
 /*   By: lylrandr <lylrandr@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 12:30:46 by lylrandr          #+#    #+#             */
-/*   Updated: 2026/03/03 12:30:47 by lylrandr         ###   ########.fr       */
+/*   Updated: 2026/03/09 16:39:07 by lylrandr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include "Iter.hpp"
+#include "Functions.hpp"
+
+int	main(){
+	int		array[5] = {1, 2 ,3, 4, 5};
+	char	array2[3] = {'a', 'b', 'c'};
+
+	std::cout << "===test 1: int array" << std::endl;
+	iter(array, 5, print<int>);
+	std::cout << "===test 2: char array" << std::endl;
+	iter(array2, 3, print<char>);
+}
